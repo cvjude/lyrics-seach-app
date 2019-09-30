a566f874172d44ca43e0529b4f1e59d52d34d2da
cvjude <jjchinosoviolet@gmail.com>
2019-09-30 13:18:26 +0100

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# lyrics-seach-app
---
+++ precache-manifest.3ae3f7c6fe64d8b0cea89721ed3c21b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eca78b86be74a7389a4c8787c18a86e4",
+    "url": "/cvjude/lyrics-seach-app/index.html"
+  },
+  {
+    "revision": "b33678a3d1ab98ebce7f",
+    "url": "/cvjude/lyrics-seach-app/static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "c384261f1f68599b7bb1",
+    "url": "/cvjude/lyrics-seach-app/static/js/2.089fb32b.chunk.js"
+  },
+  {
+    "revision": "b33678a3d1ab98ebce7f",
+    "url": "/cvjude/lyrics-seach-app/static/js/main.6e61d95f.chunk.js"
+  },
+  {
+    "revision": "361dbdcdd892cdee2340",
+    "url": "/cvjude/lyrics-seach-app/static/js/runtime-main.246d9da8.js"
+  }
+]);
